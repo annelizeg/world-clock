299dac6eabc5409f3ee8caab39104dd9e73d281e
Annelize Grobbelaar <91416875+annelizeg@users.noreply.github.com>
2025-03-12 23:24:20 +1030

    Added the functionality for the select

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
+let intervalId; // Declare at the top to maintain its scope
+let intervalId2; // Declare at the top to maintain its scope
+
 function updateCityTimeDate(city, timeZone) {
   let cityElement = document.querySelector(`#${city}`);
   let cityDateElement = cityElement.querySelector(".date");
@@ -11,13 +14,62 @@ function updateCityTimeDate(city, timeZone) {
   );
 }
 
+function displaySelectedCity(event) {
+  // Clear all previous intervals to prevent multiple intervals
+  clearInterval(intervalId);
+  clearInterval(intervalId2);
+
+  let cityTimeZone = event.target.value;
+  let cityName = event.target.value.replace("_", " ").split("/")[1];
+  let cityNameLowercase = event.target.value
+    .toLowerCase()
+    .replace("_", "-")
+    .split("/")[1];
+
+  let cityDisplayElement = document.querySelector(".cityDisplay");
+
+  // Clear previous city display content
+  cityDisplayElement.innerHTML = `<div class="city" id="${cityNameLowercase}">
+    <div>
+    <h2>${cityName}</h2>
+    <div class="date"></div>
+    </div>
+    <div class="time"></div>
+    </div>`;
+
+  console.log(cityNameLowercase);
+  console.log(cityTimeZone);
+  console.log(cityDisplayElement);
+
+  updateCityTimeDate(cityNameLowercase, cityTimeZone);
+  intervalId = setInterval(
+    updateCityTimeDate,
+    1000,
+    cityNameLowercase,
+    cityTimeZone
+  );
+}
+
 // Adelaide Time & Date
 updateCityTimeDate("adelaide", "Australia/Adelaide");
-setInterval(updateCityTimeDate, 1000, "adelaide", "Australia/Adelaide");
+intervalId = setInterval(
+  updateCityTimeDate,
+  1000,
+  "adelaide",
+  "Australia/Adelaide"
+);
 
 // New York Time & Date
 updateCityTimeDate("new-york", "America/New_York");
-setInterval(updateCityTimeDate, 1000, "new-york", "America/New_York");
+intervalId2 = setInterval(
+  updateCityTimeDate,
+  1000,
+  "new-york",
+  "America/New_York"
+);
+
+let citySelectElement = document.querySelector("#city");
+citySelectElement.addEventListener("change", displaySelectedCity);
 
 // Adelaide Time & Date
 // let adelaideElement = document.querySelector("#adelaide");
