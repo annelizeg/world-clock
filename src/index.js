5fe94df6a3ba58dd60c6fd682158ac02c6f0e84b
Annelize Grobbelaar <91416875+annelizeg@users.noreply.github.com>
2025-03-14 11:35:41 +1030

    Added homepage link, fixed click outcome on select a city

--- src/index.js
+++ src/index.js
@@ -19,40 +19,43 @@ function displaySelectedCity(event) {
   clearInterval(intervalId);
   clearInterval(intervalId2);
 
-  let cityTimeZone = event.target.value;
+  if (event.target.value.length > 0) {
+    let cityTimeZone = event.target.value;
 
-  if (cityTimeZone === "current") {
-    cityTimeZone = moment.tz.guess();
-  }
+    if (cityTimeZone === "current") {
+      cityTimeZone = moment.tz.guess();
+    }
 
-  let cityName = cityTimeZone.replace("_", " ").split("/")[1];
-  let cityNameLowercase = cityTimeZone
-    .toLowerCase()
-    .replace("_", "-")
-    .split("/")[1];
+    let cityName = cityTimeZone.replace("_", " ").split("/")[1];
+    let cityNameLowercase = cityTimeZone
+      .toLowerCase()
+      .replace("_", "-")
+      .split("/")[1];
 
-  let cityDisplayElement = document.querySelector(".cityDisplay");
+    let cityDisplayElement = document.querySelector(".cityDisplay");
 
-  // Clear previous city display content
-  cityDisplayElement.innerHTML = `<div class="city" id="${cityNameLowercase}">
+    // Clear previous city display content
+    cityDisplayElement.innerHTML = `<div class="city" id="${cityNameLowercase}">
     <div>
     <h2>${cityName}</h2>
     <div class="date"></div>
     </div>
     <div class="time"></div>
-    </div>`;
+    </div> 
+    <a href="/">All cities</a>`;
 
-  console.log(cityNameLowercase);
-  console.log(cityTimeZone);
-  console.log(cityDisplayElement);
+    console.log(cityNameLowercase);
+    console.log(cityTimeZone);
+    console.log(cityDisplayElement);
 
-  updateCityTimeDate(cityNameLowercase, cityTimeZone);
-  intervalId = setInterval(
-    updateCityTimeDate,
-    1000,
-    cityNameLowercase,
-    cityTimeZone
-  );
+    updateCityTimeDate(cityNameLowercase, cityTimeZone);
+    intervalId = setInterval(
+      updateCityTimeDate,
+      1000,
+      cityNameLowercase,
+      cityTimeZone
+    );
+  }
 }
 
 // Adelaide Time & Date
@@ -75,25 +78,3 @@ intervalId2 = setInterval(
 
 let citySelectElement = document.querySelector("#city");
 citySelectElement.addEventListener("change", displaySelectedCity);
-
-// Adelaide Time & Date
-// let adelaideElement = document.querySelector("#adelaide");
-// let adelaideDateElement = adelaideElement.querySelector(".date");
-// let adelaideTimeElement = adelaideElement.querySelector(".time");
-
-// let adelaideCurrentTime = moment().tz("Australia/Adelaide");
-// adelaideDateElement.innerHTML = adelaideCurrentTime.format("MMMM Do YYYY");
-// adelaideTimeElement.innerHTML = adelaideCurrentTime.format(
-//   "h:mm:ss [<small>]A[</small>]"
-// );
-
-// New York Time & Date
-// let newYorkElement = document.querySelector("#new-york");
-// let newYorkDateElement = newYorkElement.querySelector(".date");
-// let newYorkTimeElement = newYorkElement.querySelector(".time");
-
-// let newYorkCurrentTime = moment().tz("America/New_York");
-// newYorkDateElement.innerHTML = newYorkCurrentTime.format("MMMM Do YYYY");
-// newYorkTimeElement.innerHTML = newYorkCurrentTime.format(
-//   "h:mm:ss [<small>]A[</small>]"
-// );
